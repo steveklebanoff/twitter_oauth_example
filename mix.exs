eb0e761003b5c6935691c11992638f6f88f20930
Steve Klebanoff <steve.klebanoff@gmail.com>
2017-01-01 22:46:16 -0800

    add extwitter and update secrets files

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule TwitterOauthExample.Mixfile do
   def application do
     [mod: {TwitterOauthExample, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :extwitter]]
   end
 
   # Specifies which paths to compile per environment.
@@ -37,7 +37,10 @@ defmodule TwitterOauthExample.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:oauth, github: "tim/erlang-oauth"},
+     {:extwitter, "~> 0.7"}
+   ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
